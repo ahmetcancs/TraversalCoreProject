b14c7ca47990bb6c4de8772c7ffe7e7c77437d93
AHMET CAN <ahmetcan9816@gmail.com>
2023-04-14 02:12:59 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Destination.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{   
+    public class Destination    //Places to go in home page 
+    {
+        public int DestinationID { get; set; }
+        public string City { get; set; }
+        public string Time { get; set; }//how many stays there
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Capacity { get; set; }//How many person
+        public bool Status { get; set; } //Is holiday active?
+    }
+}
---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public int Client { get; set; }
+        public int Comment   { get; set; }
+        public int Image { get; set; }
+        public bool Status { get; set; }
+
+
+    }
+}
