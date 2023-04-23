4826f72f6ae9301037fea5bbb345df0b4508f86b
AHMET CAN <ahmetcan9816@gmail.com>
2023-04-23 03:17:45 +0300

    Registration with Identity Lib. #29

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -1,22 +1,58 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
-        [HttpGet]
+        private readonly UserManager<AppUser> _userManager;
+
+		public LoginController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpGet]
         public IActionResult SignUp()
         {
             return View();
         }
-        //[HttpPost]
-        //public IActionResult SignUp()
-        //{
-        //    return View();
-        //}
-        [HttpGet]
+
+		[HttpPost]
+		public async Task<IActionResult> SignUp(UserRegisterViewModel model)
+		{
+			AppUser appUser = new AppUser()
+			{
+				Name = model.Name,
+				Surname = model.Surname,
+				Email = model.Mail,
+				UserName = model.Username
+			};
+			if (model.Password == model.ConfirmPassword)
+			{
+				var result = await _userManager.CreateAsync(appUser, model.Password); //Remember that the password is sent by hashing.
+
+				if (result.Succeeded)
+				{
+					return RedirectToAction("SignIn");
+				}
+				else
+				{
+					foreach (var item in result.Errors)
+					{
+						ModelState.AddModelError("", item.Description);
+					}
+				}
+			}
+			return View(model);
+		}
+
+		[HttpGet]
         public IActionResult SignIn()
         {
             return View();
---
+++ TraversalCoreProject/Models/UserRegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+	public class UserRegisterViewModel
+	{
+        [Required(ErrorMessage = "Lütfen adınızı giriniz.")]
+        public string Name { get; set; }
+		
+		[Required(ErrorMessage = "Lütfen soyadınızı giriniz.")]
+		public string Surname { get; set; }
+
+		[Required(ErrorMessage = "Lütfen kullanıcı adınızı Giriniz.")]
+		public string Username { get; set; }
+
+		[Required(ErrorMessage = "Lütfen mail adresinizi Giriniz.")]
+		public string Mail { get; set; }
+
+		[Required(ErrorMessage = "Lütfen şifreyi giriniz.")]
+		public string Password { get; set; }
+
+		[Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz.")]
+		[Compare("Password",ErrorMessage = "Şifreler eşleşmiyor.")]
+		public string ConfirmPassword { get; set; }
+	}
+}
