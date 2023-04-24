d64d4cacc08f10c354e01465865bb601f5f09998
AHMET CAN <ahmetcan9816@gmail.com>
2023-04-24 17:05:41 +0300

    Asp-Validation-For added to (new) SignUp page. #30

--- TraversalCoreProject/Models/UserRegisterViewModel.cs
+++ TraversalCoreProject/Models/UserRegisterViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.VisualStudio.Web.CodeGeneration.Utils;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
 
 namespace TraversalCoreProject.Models
 {
@@ -7,7 +10,7 @@ namespace TraversalCoreProject.Models
         [Required(ErrorMessage = "Lütfen adınızı giriniz.")]
         public string Name { get; set; }
 		
-		[Required(ErrorMessage = "Lütfen soyadınızı giriniz.")]
+		[Required(ErrorMessage = "Lütfen soyadınızı giriniz.")]				
 		public string Surname { get; set; }
 
 		[Required(ErrorMessage = "Lütfen kullanıcı adınızı Giriniz.")]
@@ -16,6 +19,12 @@ namespace TraversalCoreProject.Models
 		[Required(ErrorMessage = "Lütfen mail adresinizi Giriniz.")]
 		public string Mail { get; set; }
 
+		//Here are the default password requirements in ASP.NET Core Identity
+		//Minimum length of 6 characters
+		//Requires at least one non-alphanumeric character
+		//Requires at least one uppercase letter
+		//Requires at least one lowercase letter
+		//Requires at least one digit
 		[Required(ErrorMessage = "Lütfen şifreyi giriniz.")]
 		public string Password { get; set; }
 
