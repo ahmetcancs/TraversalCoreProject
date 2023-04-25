2058a6772f54c19c2f0efe988faf82bb14c02414
AHMET CAN <ahmetcan9816@gmail.com>
2023-04-25 22:30:43 +0300

    Listing user info in profile page.

--- TraversalCoreProject/Areas/User/Controllers/ProfileController.cs
+++ TraversalCoreProject/Areas/User/Controllers/ProfileController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TraversalCoreProject.Areas.User.Models;
 
 namespace TraversalCoreProject.User.Member.Controllers
 {
+    [Area("User")]
+    [Route("User/[controller]/[action]")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public ProfileController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            var userEditViewModel = new UserEditViewModel
+            {
+                Name = values.Name,
+                Surname = values.Surname,
+                PhoneNumber = values.PhoneNumber,
+                Mail = values.Email
+            };
+            return View(userEditViewModel);
         }
     }
 }
