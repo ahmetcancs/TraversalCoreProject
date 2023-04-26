02790b8944ae8c29b316033ad5e0abf4ee6211d8
AHMET CAN <ahmetcan9816@gmail.com>
2023-04-27 02:08:22 +0300

    Reservation procedures created.

---
+++ BusinessLayer/Concrete/ReservationManager.cs
@@ -0,0 +1,77 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ReservationManager : IReservationService
+    {
+        IReservationDal reservationDal;
+
+        public ReservationManager(IReservationDal reservationDal)
+        {
+            this.reservationDal = reservationDal;
+        }
+
+        public void Delete(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation GetbyID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> GetListByFilter(Expression<Func<Reservation, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Reservation t)
+        {
+            reservationDal.Insert(t);
+        }
+
+        public void TDelete(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TraversalCoreProject/Areas/User/Controllers/ReservationController.cs
+++ TraversalCoreProject/Areas/User/Controllers/ReservationController.cs
@@ -1,11 +1,18 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TraversalCoreProject.Areas.User.Controllers
 {
     [Area("User")]
     public class ReservationController : Controller
     {
+        DestinationManager _destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager _reservationManager = new ReservationManager(new EfReservationDal());
         public IActionResult MyCurrentReservation()
         {
             return View();
@@ -16,14 +23,24 @@ namespace TraversalCoreProject.Areas.User.Controllers
         }
         [HttpGet]
         public IActionResult NewReservation()
-        {
+        {      // Here will change
+            List<SelectListItem> values = (from x in _destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
             return View();
         }
 
         [HttpPost]
         public IActionResult NewReservation(Reservation reservation)
         {
-            return View();
+            reservation.Status = "Onay Bekliyor";
+            reservation.AppUserId = 5;
+            _reservationManager.TAdd(reservation);
+            return RedirectToAction("MyCurrentReservation");
         }
     }
 }
