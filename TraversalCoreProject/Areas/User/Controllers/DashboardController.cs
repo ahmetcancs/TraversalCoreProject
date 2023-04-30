78e2160363a60a76117369f6fccfad9e495053d0
AHMET CAN <ahmetcan9816@gmail.com>
2023-04-30 23:34:17 +0300

    User profile is created.

---
+++ TraversalCoreProject/Areas/User/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.Areas.User.Controllers
+{
+    [Area("User")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.Name = values.Name + " " + values.Surname;
+            ViewBag.Image = values.ImageUrl;
+            return View();
+        }
+    }
+}
+    
