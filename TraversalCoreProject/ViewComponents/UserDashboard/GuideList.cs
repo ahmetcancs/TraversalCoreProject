f2f68e9e9d8490dc26de76dec95db8ec5f542dfd
AHMET CAN <ahmetcan9816@gmail.com>
2023-05-03 23:50:37 +0300

    Profile Info listed.

---
+++ TraversalCoreProject/ViewComponents/UserDashboard/GuideList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.UserDashboard
+{
+    public class GuideList : ViewComponent
+    {
+        GuideManager guideManager = new GuideManager(new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = guideManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/UserDashboard/ProfileInformation.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.UserDashboard
+{
+    public class ProfileInformation : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task <IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name + " " + values.Surname;
+            ViewBag.phone = values.PhoneNumber;
+            ViewBag.email = values.Email;
+            return View();
+        }
+    }
+}
+    
