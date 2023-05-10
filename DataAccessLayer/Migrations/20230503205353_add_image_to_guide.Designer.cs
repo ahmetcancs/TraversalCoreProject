7ca7fcc13cd60b682c4a9d25566c5f00725fd9cf
AHMET CAN <ahmetcan9816@gmail.com>
2023-05-10 23:30:21 +0300

    Updated.

--- DataAccessLayer/Migrations/20230503205353_add_image_to_guide.Designer.cs
+++ DataAccessLayer/Migrations/20230503205353_add_image_to_guide.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230503205353_add_image_to_guide")]
+    partial class add_image_to_guide
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
