fcd6ab7a0dd75879c0e54b9f25df77d5cf304ef0
AHMET CAN <ahmetcan9816@gmail.com>
2023-05-28 23:17:32 +0300

    Guide List methods  added. (adding form) #57

--- TraversalCoreProject/Areas/Admin/Controllers/GuideController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/GuideController.cs
@@ -19,5 +19,43 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
             var values = _guideService.TGetList();
             return View(values);
         }
+        [HttpGet]
+        public IActionResult AddGuide()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddGuide(Guide guide)
+        {
+            _guideService.TAdd(guide);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditGuide(int id)
+        {
+            var values = _guideService.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditGuide(Guide guide)
+        {
+            _guideService.TUpdate(guide);
+            return RedirectToAction("Index");
+        }
+        public IActionResult ChangeStatus(int id)
+        {
+            var value = _guideService.TGetByID(id);
+            if (value.Status == true)
+            {
+                value.Status = false;
+                _guideService.TUpdate(value);
+            }
+            else
+            {
+                value.Status = true;
+                _guideService.TUpdate(value);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
