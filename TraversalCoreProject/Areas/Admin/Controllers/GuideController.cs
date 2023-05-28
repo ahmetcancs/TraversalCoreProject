98b807f11d15df3775defee9614060cc04267675
AHMET CAN <ahmetcan9816@gmail.com>
2023-05-28 23:06:08 +0300

    Guide List created. #56.8

---
+++ TraversalCoreProject/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _guideService.TGetList();
+            return View(values);
+        }
+    }
+}
