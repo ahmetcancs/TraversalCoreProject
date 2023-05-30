c83e1f5be63c8df25533ddf773d73ab0345ce799
AHMET CAN <ahmetcan9816@gmail.com>
2023-05-30 03:21:42 +0300

    Static-Dynamic Excel Report method added for Destination #58.9

---
+++ TraversalCoreProject/Controllers/ExcelController.cs
@@ -0,0 +1,96 @@
+﻿using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class ExcelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<DestinationModel> DestinationList()
+        {
+            List<DestinationModel> destinationModels = new List<DestinationModel>();
+            using (var c = new Context())
+            {
+                destinationModels = c.Destinations.Select(x => new DestinationModel
+                {
+                    City = x.City,
+                    Time = x.Time,
+                    Price = x.Price,
+                    Capacity = x.Capacity
+                }).ToList();
+            }
+            return destinationModels;
+        }
+
+        public IActionResult StaticExcelReport()
+        {
+            ExcelPackage excel = new ExcelPackage();
+            var worksheet = excel.Workbook.Worksheets.Add("Page1");
+            worksheet.Cells[1, 1].Value = "Rota";
+            worksheet.Cells[1, 2].Value = "Rehber";
+            worksheet.Cells[1, 3].Value = "Kontenjan";
+
+            worksheet.Cells[2, 1].Value = "İstanbul Turu";
+            worksheet.Cells[2, 2].Value = "Ahmet Can";
+            worksheet.Cells[2, 3].Value = "50";
+
+            worksheet.Cells[3, 1].Value = "Erzurum Turu";
+            worksheet.Cells[3, 2].Value = "Yiğit Can Şanlı";
+            worksheet.Cells[3, 3].Value = "40";
+
+            var bytes = excel.GetAsByteArray();
+            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "File1.xlsx");
+        }
+
+        public IActionResult DestinationExcelReport()
+        {
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Tur Listesi");
+                workSheet.Cell(1, 1).Value = "Şehir";
+                workSheet.Cell(1, 2).Value = "Konaklama Süresi";
+                workSheet.Cell(1, 3).Value = "Fiyat";
+                workSheet.Cell(1, 4).Value = "Kapasite";
+
+                for (int i = 1; i <= workSheet.ColumnCount(); i++)
+                {
+                    workSheet.Cell(1, i).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                }
+
+                var rowCount = 2;
+                foreach (var item in DestinationList())
+                {
+                    workSheet.Cell(rowCount, 1).Value = item.City;
+                    workSheet.Cell(rowCount, 2).Value = item.Time;
+                    workSheet.Cell(rowCount, 3).Value = item.Price;
+                    workSheet.Cell(rowCount, 4).Value = item.Capacity;
+
+                    for (int i = 1; i <= workSheet.ColumnCount(); i++)
+                    {
+                        workSheet.Cell(rowCount, i).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                    }
+
+                    rowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "NewList.xlsx");
+                }
+            }
+        }
+    }
+}
---
+++ TraversalCoreProject/Models/DestinationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TraversalCoreProject.Models
+{
+    public class DestinationModel
+    {
+        public string City { get; set; }
+        public string Time { get; set; }
+        public double Price { get; set; }
+        public string Capacity { get; set; }
+    }
+}
